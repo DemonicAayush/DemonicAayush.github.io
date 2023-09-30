82a71bb3e935f23c33f71f98c0e53fee7e72b457
DemonKingSwarn <rockingswarn@gmail.com>
2023-09-30 19:07:09 +0530

    some changes

--- script.js
+++ script.js
@@ -1,2 +1,13 @@
 
 // You can add interactive features or functionality here if needed
+
+
+document.addEventListener("DOMContentLoaded", function () {
+  const killedGameItems = document.querySelectorAll(".killedGame");
+
+  killedGameItems.forEach((item, index) => {
+    // Delay the animation for each item
+    item.style.animation = `fadeIn 1s ${index * 0.3}s both`;
+  });
+});
+
