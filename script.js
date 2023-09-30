4986460788fae08920bd51d21af3c9edb5301849
DemonKingSwarn <rockingswarn@gmail.com>
2023-09-30 19:31:47 +0530

    pop up mhm yes

--- script.js
+++ script.js
@@ -1,28 +1,21 @@
-document.addEventListener("DOMContentLoaded", function () {
-  const killedGameItems = document.querySelectorAll(".killedGame");
+// JavaScript code to open a pop-up on hover
+const gameItems = document.querySelectorAll(".game");
 
-  killedGameItems.forEach((item) => {
-    createParticles(item);
+// Remove the click event listener
+gameItems.forEach((item) => {
+  item.addEventListener("mouseover", () => {
+    // You can add code to display more details about the game in a pop-up here
+    const gameDetails = item.querySelector(".game-details");
+    if (gameDetails) {
+      gameDetails.style.display = "block";
+    }
+  });
+
+  item.addEventListener("mouseout", () => {
+    const gameDetails = item.querySelector(".game-details");
+    if (gameDetails) {
+      gameDetails.style.display = "none";
+    }
   });
 });
-
-function createParticles(container) {
-  for (let i = 0; i < 10; i++) {
-    const particle = document.createElement("span");
-    particle.className = "particle";
-    container.appendChild(particle);
-
-    const size = Math.random() * 20 + 10;
-    const duration = Math.random() * 2 + 1;
-    const delay = Math.random() * 0.5;
-
-    particle.style.width = size + "px";
-    particle.style.height = size + "px";
-    particle.style.animation = `moveParticle ${duration}s ease-in-out ${delay}s infinite`;
-
-    setTimeout(() => {
-      container.removeChild(particle);
-    }, duration * 1000);
-  }
-}
 
