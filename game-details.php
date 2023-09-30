66059cdab35f0ac343c5f40ae37f999a46f71f45
DemonKingSwarn <rockingswarn@gmail.com>
2023-10-01 00:55:13 +0530

    implemented ajax

---
+++ game-details.php
@@ -0,0 +1,35 @@
+<?php
+// Sample array of game details (you should replace this with your actual data source)
+$gameDetailsData = array(
+    "Confined" => array(
+        "status" => "Killed",
+        "description" => "Confined was a horror game being developed by DemonKingSwarn but eventually he stopped working on it because he started getting weary of it."
+    ),
+    "ManagerCEO" => array(
+        "status" => "Killed",
+        "description" => "Nikhil Malankar gave the ownership of this game to DemonKingSwarn who then gave it to Aayush who then killed the game out of his lack of interest."
+    ),
+    "URI" => array(
+        "status" => "Killed",
+        "description" => "One of the first group projects of Aayush(cloudy) at GameOH. Later got killed due to skill issue. I really liked its aesthetics, 2D lights - good old days of working in 2D."
+    ),
+    "Avoid - Reborn" => array(
+        "status" => "Killed",
+        "description" => "Full version of Aayush's popular game - <a href='https://cloudyin.itch.io/avoid' target='_blank'>Avoid</a>. Avoid also happens to be his first published game in Brackeys game jam."
+    )
+);
+
+function getGameDetails($title) {
+    global $gameDetailsData;
+
+    // Check if the game title exists in the data
+    if (array_key_exists($title, $gameDetailsData)) {
+        $details = "<p>Status: " . $gameDetailsData[$title]["status"] . "</p>";
+        $details .= "<p>" . $gameDetailsData[$title]["description"] . "</p>";
+        return $details;
+    } else {
+        return "Game details not found.";
+    }
+}
+?>
+
--- script.js
+++ script.js
@@ -19,3 +19,33 @@ gameItems.forEach((item) => {
   });
 });
 
+$(document).ready(function () {
+    // Select all game cards
+    var gameCards = $(".game");
+
+    // Add a click event listener to each game card
+    gameCards.each(function () {
+        $(this).click(function () {
+            // Check if the game details are already loaded
+            if ($(this).find(".game-details").is(":empty")) {
+                // Get the game title
+                var gameTitle = $(this).find("h3").text();
+
+                // Send an AJAX request to fetch game details
+                $.ajax({
+                    url: "game-details.php", // Replace with the actual URL of your server-side script
+                    type: "GET",
+                    data: { title: gameTitle },
+                    success: function (data) {
+                        // Update the game details in the card
+                        $(this).find(".game-details").html(data);
+                    }.bind(this), // Ensure 'this' refers to the clicked game card
+                    error: function () {
+                        alert("Failed to fetch game details.");
+                    },
+                });
+            }
+        });
+    });
+});
+
