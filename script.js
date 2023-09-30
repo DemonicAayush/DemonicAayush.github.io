e0eab783820c36dfcc43ae2b5ad86a2efe678cef
DemonKingSwarn <rockingswarn@gmail.com>
2023-09-30 19:11:26 +0530

    animations added

--- script.js
+++ script.js
@@ -1,13 +1,28 @@
-
-// You can add interactive features or functionality here if needed
-
-
 document.addEventListener("DOMContentLoaded", function () {
   const killedGameItems = document.querySelectorAll(".killedGame");
 
-  killedGameItems.forEach((item, index) => {
-    // Delay the animation for each item
-    item.style.animation = `fadeIn 1s ${index * 0.3}s both`;
+  killedGameItems.forEach((item) => {
+    createParticles(item);
   });
 });
+
+function createParticles(container) {
+  for (let i = 0; i < 10; i++) {
+    const particle = document.createElement("span");
+    particle.className = "particle";
+    container.appendChild(particle);
+
+    const size = Math.random() * 20 + 10;
+    const duration = Math.random() * 2 + 1;
+    const delay = Math.random() * 0.5;
+
+    particle.style.width = size + "px";
+    particle.style.height = size + "px";
+    particle.style.animation = `moveParticle ${duration}s ease-in-out ${delay}s infinite`;
+
+    setTimeout(() => {
+      container.removeChild(particle);
+    }, duration * 1000);
+  }
+}
 
